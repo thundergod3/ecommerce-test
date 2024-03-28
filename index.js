98827e3a074e272a2e1950e6faa605a7d1657631
Cong Phan <congthundergodphan@gmail.com>
2024-03-28 23:17:19 +0700

    Setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const crypto = require("crypto");
+
+const PORT = 8000;
+
+const hmac = crypto.createHmac("sha256", "sha256-hmac");
+const app = express();
+
+app.get("/sha256-hmac", async (req, res) => {
+  try {
+    const { partner_id, path } = req.query;
+    const timestamp = new Date().getTime();
+    const baseString = `${partner_id}${path}${timestamp}`;
+    const data = hmac.update(baseString).digest("hex");
+
+    res.send({ data, err: false });
+  } catch (err) {
+    res.send({ data: false, err: err.toString() });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started At ${PORT}`);
+});
